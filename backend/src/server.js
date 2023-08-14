a52c3521d85b9ee4e9bc20cf7ffd5a94cb5cf896
Utkarsh Gill <ugill@Utkarshs-MacBook-Pro.local>
2023-08-14 12:26:14 +0530

    Enabled admin side, login, home.

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,57 +17,18 @@ app.get('/', (req, res) => {
     image: catUrl
   };
   res.json(catJson);
-  
+
 });
 
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
 let lastResponse = null;
 
-app.post('/generate', async (req, res) => {
-  const prompt = req.body.prompt;
-  const prevJSON = req.body.prevJSON;
-
-  let inputPrompt = prompt;
-  if (lastResponse != null) {
-    // inputPrompt = prompt + '*/' + +  '\n\n /*Return a JSON in this format {"html": "<!DOCTYPE html>", "css": "",}*/';
-    inputPrompt = prompt;
-    const result = await openai.createEdit({
-      input: JSON.stringify(lastResponse),
-      instruction: inputPrompt,
-      model:'text-davinci-edit-001',
-      n:1
-    });
-    console.log('has a last response');
-    const response = result.data.choices[0].text;
-    
-    res.json(JSON.parse(response));
-    lastResponse = JSON.parse(response);
-    
-  } else {
-    inputPrompt =  prompt +  '\n\n /* Return a JSON in this format {"html": "", "css": "",}*/';
-    const result = await openai.createCompletion({
-      max_tokens : 500,
-      n:1,
-      model: "text-davinci-003",
-      prompt: inputPrompt
-    });
-
-    const response = result.data.choices[0].text;
-    
-  res.json(JSON.parse(response));
-  lastResponse = JSON.parse(response);
-  }
-
-
-  console.log('response is: '+lastResponse.text);
- 
-});
 
 
 
